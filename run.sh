8dfc9b6a49b1fd3546df2a684a12e37f6d526608
mobius-chenw <mobius.chenw@gmail.com>
2016-05-02 11:37:41 +0800

    11

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ while true
 do
 
 	echo > /data/log.txt
-	echo -e "*******************************************\n" >> /data/log.txt
-	export TZ=Asia/Shanghai && date >> /data/log.txt
+	echo -e "*******************************************" >> /data/log.txt
+	# export TZ=Asia/Shanghai && date >> /data/log.txt
 	mtr -r -4 -c 10 baidu.com >> /data/log.txt
-	echo -e "******************************************\n" >> /data/log.txt
-	sleep 5
+	echo -e "\n******************************************\n" >> /data/log.txt
+	sleep 10
 
 done
